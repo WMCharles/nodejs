8bb5c78923e267fec5cd44f8eb25e79f1beeb0a6
wmcharles <charleswafula73@gmail.com>
2023-02-18 20:57:41 +0300

    add post request

--- src/app.js
+++ src/app.js
@@ -1,5 +1,10 @@
 const express = require('express');
 const app = express();
+
+// for uploading data
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 const PORT = 3000
 
 // data 
@@ -22,13 +27,20 @@ app.get('/', (req, res, next) => {
     res.send("Welcome!");
 })
 
+app.post('/', (req, res, next) => {
+    res.send("Hello This is a POST request!")
+})
+
+// CUSTOMERS
+// GET /customers
 app.get('/api/customers', (req, res, next) => {
     res.send({ "customers": people });
 })
 
-app.post('/', (req, res, next) => {
-    res.send("Hello This is a POST request!")
-})
+// POST /customers
+app.post('/api/customers', (req, res) => {
+    res.send(req.body)
+});
 
 // message on terminal
 app.listen(PORT, () => {
